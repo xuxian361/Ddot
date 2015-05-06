74c811a26ce25bdf38a61f6dd8add8401c388f43
Sundy_Xu <sundy_xu@mtelnet.com>
2015-05-06 10:25:03 +0800

    update setting help info

--- Ddot/src/com/sundy/Ddot/ui/fragment/HelpInfoFragment.java
+++ Ddot/src/com/sundy/Ddot/ui/fragment/HelpInfoFragment.java
@@ -1,26 +1,33 @@
 package com.sundy.Ddot.ui.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.androidquery.AQuery;
-import com.lidroid.xutils.util.LogUtils;
 import com.sundy.Ddot.R;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 /**
- * Created by sundy on 15/5/2.
+ * Created by sundy on 15/5/6.
  */
-public class SettingsFragment extends BaseFragment {
+public class HelpInfoFragment extends BaseFragment {
 
-    private final String TAG = "SettingsFragment";
+    private final String TAG = "HelpInfoFragment";
     private View v;
+    private int type = 0;
+    private WebView webView;
 
-    public SettingsFragment() {
+    public HelpInfoFragment() {
+    }
+
+    public HelpInfoFragment(int type) {
+        this.type = type;
     }
 
     @Override
@@ -30,24 +37,40 @@ public class SettingsFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.d_settings, container, false);
+        v = inflater.inflate(R.layout.d_help_info, container, false);
 
         aq = new AQuery(v);
         init();
+
         return v;
     }
 
     private void init() {
-        aq.id(R.id.txt_header_title).text(getString(R.string.settings));
         aq.id(R.id.btnBack).clicked(onClick);
         aq.id(R.id.linear_all).clicked(onClick);
 
-        aq.id(R.id.txt_service_flow).clicked(onClick);
-        aq.id(R.id.txt_terms).clicked(onClick);
-        aq.id(R.id.txt_normal_question).clicked(onClick);
-        aq.id(R.id.txt_contact_us).clicked(onClick);
-        aq.id(R.id.txt_about_us).clicked(onClick);
+        webView = aq.id(R.id.webView).getWebView();
+        if (type == 1) {
+            webView.loadUrl("http://www.baidu.com");
+        } else if (type == 2) {
+            webView.loadUrl("http://www.weibo.com/");
+        } else if (type == 3) {
+            webView.loadUrl("https://www.google.com.hk/");
+        } else if (type == 4) {
+            webView.loadUrl("https://github.com/");
+        } else if (type == 5) {
+            webView.loadUrl("http://www.imooc.com/");
+        }
 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true;
+            }
+        });
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
     }
 
@@ -57,21 +80,6 @@ public class SettingsFragment extends BaseFragment {
             switch (view.getId()) {
                 case R.id.btnBack:
                     onBackPressed();
-                    break;
-                case R.id.txt_service_flow:
-                    addContent(new HelpInfoFragment(1));
-                    break;
-                case R.id.txt_terms:
-                    addContent(new HelpInfoFragment(2));
-                    break;
-                case R.id.txt_normal_question:
-                    addContent(new HelpInfoFragment(3));
-                    break;
-                case R.id.txt_contact_us:
-                    addContent(new HelpInfoFragment(4));
-                    break;
-                case R.id.txt_about_us:
-                    addContent(new HelpInfoFragment(5));
                     break;
             }
         }
