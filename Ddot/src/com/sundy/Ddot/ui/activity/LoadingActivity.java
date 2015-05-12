c7de7f59d194b9dffb4c9b3de11d99e8e86643ed
Sundy_Xu <sundy_xu@mtelnet.com>
2015-05-12 11:04:23 +0800

    update Facebook version

--- Ddot/src/com/sundy/Ddot/ui/activity/LoadingActivity.java
+++ Ddot/src/com/sundy/Ddot/ui/activity/LoadingActivity.java
@@ -8,15 +8,22 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import com.androidquery.AQuery;
+import com.facebook.AppEventsLogger;
+import com.lidroid.xutils.util.LogUtils;
 import com.sundy.Ddot.R;
 import com.sundy.Ddot.utils.Constant;
 import com.sundy.Ddot.utils.Utils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoadingActivity extends Activity {
 
     private static final int GO_HOME = 998;
     private static final int GO_GUIDE = 999;
     private static final long SPLASH_DELAY_MILLIS = 1000;
+    private AQuery aq;
 
     private Handler mHandler = new Handler() {
         @Override
@@ -44,12 +51,36 @@ public class LoadingActivity extends Activity {
         Constant.SCREEN_HEIGHT = metrics.heightPixels;
         Constant.SCREEN_DENSITY = metrics.density;
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                init();
+        aq = new AQuery(this);
+        getBanner();
+    }
+
+    private void getBanner() {
+        String str = new String("{\n" +
+                "    \"Message\": \"Success\", \n" +
+                "    \"FF\": {\n" +
+                "        \"banner\": \"http://img6.cache.netease.com/cnews/2015/5/11/201505110752240f45a.jpg\"\n" +
+                "    }, \n" +
+                "    \"Result\": 0\n" +
+                "}");
+        try {
+            JSONObject object = new JSONObject(str);
+            if (object.has("FF")) {
+                JSONObject FF = object.getJSONObject("FF");
+                if (FF != null) {
+                    String banner = FF.getString("banner");
+                    aq.id(R.id.img).image(banner);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            init();
+                        }
+                    }, 1000);
+                }
             }
-        }, 1000);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     private void init() {
@@ -76,5 +107,17 @@ public class LoadingActivity extends Activity {
         intent.putExtra("isFromSetting", false);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
     }
 }
